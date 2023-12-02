af871ea0a029e631dbc5802e97e6a9b28f4d11ec
jhony2488 <jhon.araujo2488@gmail.com>
2023-12-02 17:38:22 -0300

    configurado autenticacao de admin user

--- src/app/middlewares/admin-auth.ts
+++ src/app/middlewares/admin-auth.ts
@@ -1,33 +1,46 @@
-import {NextFunction, Request, Response} from 'express';
+import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+
+import { UserRepository } from '../repositories/Users';
 
 if (process.env.NODE_ENV === 'development') {
   require('dotenv').config({
     path: '.env.development',
-  })
+  });
 } else {
   require('dotenv').config({
     path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
-  })
+  });
 }
 async function auth(req: Request, res: Response, next: NextFunction) {
-  const authHeader:string | undefined = req.header('Authorization');
+  const authHeader: string | undefined = req.header('Authorization');
+  const authUserEmail: string | undefined = req.header('email');
+  let errorUserAdmin = false;
+
+  const User = new UserRepository();
+
+  await User.findByEmail(authUserEmail).then((user) => {
+    if (user[0].rule !== 'admin') {
+      errorUserAdmin = true;
+    }
+  });
+
+  if (errorUserAdmin) {
+    return res.status(401).json({ message: 'Token not provided' });
+  }
 
   if (!authHeader) {
     return res.status(401).json({ message: 'Token not provided' });
   }
-  const token: string = req.header('Authorization')
+  const token: string = req.header('Authorization');
   const jwtApp = await jwt.sign(
     {
       token: 'Bearer ' + token,
     },
-    token
+    token,
   );
   try {
-    const verified = await jwt.verify(
-      jwtApp,
-      'Bearer ' + process.env.APP_SECRET
-    );
+    const verified = await jwt.verify(jwtApp, 'Bearer ' + process.env.APP_SECRET);
     if (verified) {
       next();
     }
