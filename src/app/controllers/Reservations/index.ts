a493798f9a8dd3c2d97b8b5e5019bc2032acd8b6
jhony2488 <jhon.araujo2488@gmail.com>
2023-12-02 19:43:36 -0300

    config routes

--- src/app/controllers/Reservations/index.ts
+++ src/app/controllers/Reservations/index.ts
@@ -2,6 +2,6 @@ import GetReservations from './GetReservations';
 import SetReservations from './SetReservations';
 import UpdateReservations from './UpdateReservations';
 import DeleteReservations from './DeleteReservations';
-import UpdateHourReservation from './UpdateHourReservation';
+import ReservationVerify from './ReservationVerify';
 
-export { GetReservations, SetReservations, UpdateReservations, DeleteReservations,UpdateHourReservation  };
+export { GetReservations, SetReservations, UpdateReservations, DeleteReservations,ReservationVerify  };
