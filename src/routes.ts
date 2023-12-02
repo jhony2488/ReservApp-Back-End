2dfe793b1a41d7c3b69adc23475a0cb43eafcab7
jhony2488 <jhon.araujo2488@gmail.com>
2023-12-02 17:40:01 -0300

    configurado rotas

--- src/routes.ts
+++ src/routes.ts
@@ -11,26 +11,63 @@ import {
   UpdateHourReservation
 } from './app/controllers/Reservations';
 
+import {
+ GetUsers,
+ Login,
+ UpdateUser,
+ SetUser,
+ DeleteUser,
+ UpdateRuleUser
+} from './app/controllers/User';
+
+import {
+ GetIncentives,
+ SetIncentives,
+ UpdateIncentives,
+ DeleteIncentive
+ } from './app/controllers/Incentives';
+
+ import {
+ GetTables,
+ SetTables,
+ UpdateTable,
+ DeleteTable
+  } from './app/controllers/Tables';
+
 const router = express.Router();
 
 //default
 router.get('/', authentication, DefaultControllersUsers);
-/*
-//user
-router.get('/users', authentication, GetReservations);
-router.post('/users', authentication, SetReservations);
-router.put('/users/:id', authentication, UpdateReservations);
-router.patch('/users/:id', authentication, UpdateHourReservation);
-router.delete('/users/:id', authentication, DeleteReservations);
 
-//reservation
+//reservations
 router.get('/reservations', adminAuth, GetReservations);
 router.post('/reservations', authentication, SetReservations);
 router.put('/reservations/:id', authentication, UpdateReservations);
-router.patch('/reservations/:id', authentication, UpdateHourReservation);
 router.delete('/reservations/:id', authentication, DeleteReservations);
 
 //reservation verify hours
 router.post('/reservation-verify', authentication, UpdateHourReservation);
-*/
+
+//user
+router.get('/users', authentication, GetUsers);
+router.post('/user/login', authentication, Login);
+router.post('/users', authentication, SetUser);
+router.put('/users/:id', authentication, UpdateUser);
+router.patch('/users/:id', authentication, UpdateRuleUser);
+router.delete('/users/:id', authentication, DeleteUser);
+
+//Incentives
+router.get('/incentives', authentication, GetIncentives);
+router.post('/incentives', authentication, SetIncentives);
+router.put('/incentives/:id', authentication, UpdateIncentives);
+router.delete('/incentives/:id', authentication, DeleteIncentive);
+
+//Tables
+router.get('/incentives', authentication, GetTables);
+router.post('/incentives', authentication, SetTables);
+router.put('/incentives/:id', authentication, UpdateTable);
+router.delete('/incentives/:id', authentication,  DeleteTable);
+
+
+
 export default router;
